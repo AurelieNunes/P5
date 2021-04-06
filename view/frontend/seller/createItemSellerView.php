aacd8ab4b8ea6f4cec8ba72afba179ae9f54c25a
AurelieNunes <aurelie.nunes.dev@gmail.com>
2021-04-06 20:55:14 +0200

    create part category

--- view/frontend/seller/createItemSellerView.php
+++ view/frontend/seller/createItemSellerView.php
@@ -2,6 +2,7 @@
 
 <?php ob_start(); ?>
 
+
 <section id="createItem" class="mx-auto pt-5">
     <h1 class="text-center">Ajout Article</h1>
     <div id="managerBlock" class="mx-auto">
@@ -11,30 +12,55 @@
                 enctype="multipart/form-data">
 
                 <label for="ref">Référence</label>
-                <input class="mb-2 col-2" name="ref" id="ref"/>
+                <input class="mb-2 col-2" name="ref" id="ref" />
 
                 <label for="name">Nom du produit : </label>
-                <input class="mb-2 col-6" type="text" name="nameItem" id="nameItem"/>
+                <input class="mb-2 col-6" type="text" name="nameItem" id="nameItem" />
 
                 <label for="description">Description </label>
                 <textarea name="descriptionItem" rows="20" cols="40" class="mb-3"></textarea>
 
                 <div class="infos d-flex">
                     <label for="price" class="mr-2">Prix</label>
-                    <input class="mb-2 col-2 mr-2" type="number" name="price" id="price"/>
+                    <input class="mb-2 col-2 mr-2" type="number" name="price" id="price" />
 
                     <label for="size" class="mr-2">Taille</label>
-                    <input class="mb-2 col-2 mr-2" type="text" name="size" id="size"/>
+                    <input class="mb-2 col-2 mr-2" type="text" name="size" id="size" />
 
                     <label for="stock" class="mr-2">Stock</label>
                     <input class="mb-2 col-2 mr-2" type="number" name="stock" id="stock" />
                 </div>
-                <label for="img" class="mt-5">Importer une image</label>
-                <input type="file" class="mb-3" name="picture" />
-
-                <button class="mb-5 col-4 mx-auto" type="submit" value="Ajouter">Ajouter</button>
-            </form>
+                <div class="img&category col-10 d-flex align-center justify-content-between align-items-center mx-auto">
+                    <div>
+                        <label for="img" class="mt-5">Importer une image</label>
+                        <input type="file" class="mb-3" name="picture" />
+                    </div>
+                    <div class="bloc w-50">
+                        <div class="select w-50">
+                            <select>
+                                <option>Catégorie</option>
+                                <option value="1">Ameublement</option>
+                                <option value="2">Bijouterie</option>
+                                <option value="3">Caviste</option>
+                                <option value="4">Cosmétique</option>
+                                <option value="5">Décoration</option>
+                                <option value="6">Epicerie fine</option>
+                                <option value="7">Librairie</option>
+                                <option value="8">Maroquinerie</option>
+                                <option value="9">Mercerie</option>
+                                <option value="10">Minéraux</option>
+                                <option value="11">Mode</option>
+                                <option value="12">Puériculture</option>
+                                <option value="13">Souvenirs</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
         </div>
+
+        <button class="mb-5 col-2 mx-auto d-flex justify-content-center" type="submit" value="Ajouter">Ajouter</button>
+        </form>
+    </div>
     </div>
 </section>
 
